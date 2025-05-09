c353ecb7c263a39f195dc83d10e7887291a70f78
Kazuki Shin <shinkazuki99@gmail.com>
2025-05-09 16:41:50 -0700

    restart

--- apps/api-gateway/src/routes/health.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-
-const router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ status: 'UP', message: 'API Gateway health is OK!' });
-});
-
-export default router; 
--- apps/web/jest.setup.js
+++
@@ -1,5 +0,0 @@
-import { toHaveNoViolations } from 'jest-axe';
-
-expect.extend(toHaveNoViolations);
-
-// You can add other global test setup here if needed 
